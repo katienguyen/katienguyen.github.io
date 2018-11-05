ab9f3d1ae8838b5dee772814ce638a23af2fe9e7
Trinh Nguyen Thi Tuyet <Trinh.NguyenThiTuyet1@nashtechglobal.com>
2018-11-05 08:47:30 +0700

    Calculator

---
+++ precache-manifest.0d85ab1756a781ec558f1f7ad9e51880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0688b25bcf1852c7fe90",
+    "url": "/static/js/main.0688b25b.chunk.js"
+  },
+  {
+    "revision": "49d3a8fcd129cca45416",
+    "url": "/static/js/1.49d3a8fc.chunk.js"
+  },
+  {
+    "revision": "0688b25bcf1852c7fe90",
+    "url": "/static/css/main.e2c6745b.chunk.css"
+  },
+  {
+    "revision": "9452e6eec1df070d74ad8afaade4ceec",
+    "url": "/index.html"
+  }
+];
